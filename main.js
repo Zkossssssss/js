83bfe5e35333a89a91be417c706c23f6da8233a6
Konstantin <k_bobrovski03@mail.ru>
2022-02-16 12:52:38 +0700

    4

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-function show(){
-	let age=parseInt(document.getElementById('age').value)
-	let div=document.getElementById('enner')
+let arr = {}
+const n = 10
 
-
-	if(age<17){
-		
-		div.innerHTML='<b>Вы подросток</b>'
-	}
-	else if (age<10){
-		
-		div.innerHTML='<b>Вы ребенок</b>'
-	}
-	else if (age>60){
-		
-		div.innerHTML='<b>вы пожилой</b>'
-	}
-	else {
-		
-		div.innerHTML='<b>вы взрослый</b>'
-
-	}
-} 
+for (let i = 0; i<n; i++){
+	arr[i]=(Math.floor(Math.random()*10)
+}
+ 
+console.log(arr) 
