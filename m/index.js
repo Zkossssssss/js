d401d85e35ed3072a47b3c2239b7fccb060e2f17
Konstantin <k_bobrovski03@mail.ru>
2022-02-25 13:00:08 +0700

    p.2

--- m/index.js
+++ m/index.js
@@ -1,5 +1,10 @@
-let x = {
-	'name':'Rustam', 
-	'age':18,
-	'height':160.5
-}
+let students = [
+{'id':1, 'name':'Danila','surname':'Alferov'},
+{'id':2, 'name':'Tihon','surname':'Balaganski'},
+{'id':3, 'name':'Konstantin','surname':'Bobrovski'},
+{'id':4, 'name':'Vladimir','surname':'Burikin'},
+{'id':5, 'name':'Semen','surname':'Veligjanin'}
+]
+for (let i=0; i<students.length; i++) {
+	console.log(students[i].surname+' '+students[i].name)
+} 
