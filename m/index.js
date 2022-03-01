b049040330b4c6463a0cf37e7ba5faa8f82ad5db
Konstantin <k_bobrovski03@mail.ru>
2022-03-01 18:03:06 +0700

    1.3

--- m/index.js
+++ m/index.js
@@ -1,10 +1,31 @@
+
 let students = [
-{'id':1, 'name':'Danila','surname':'Alferov'},
-{'id':2, 'name':'Tihon','surname':'Balaganski'},
-{'id':3, 'name':'Konstantin','surname':'Bobrovski'},
-{'id':4, 'name':'Vladimir','surname':'Burikin'},
-{'id':5, 'name':'Semen','surname':'Veligjanin'}
+	{'id': 1, 'name': 'Данила', 'surname': 'Алферов'},
+	{'id': 2, 'name': 'Тихон', 'surname': 'Балаганский'},
+	{'id': 3, 'name': 'Константин', 'surname': 'Бобровский'},
+	{'id': 4, 'name': 'Владимир', 'surname': 'Бурыкин'},
+	{'id': 5, 'name': 'Семён', 'surname': 'Велигжанин'}
 ]
-for (let i=0; i<students.length; i++) {
-	console.log(students[i].surname+' '+students[i].name)
-} 
+
+let current = 0
+function load_student(id) {
+	document.getElementById('name').value = students[id].name
+	document.getElementById('surname').value = students[id].surname
+}
+function next() {
+	document.getElementById('button vlevo').disabled = false
+	current++
+	load_student(current)
+	if (current == 4) {
+		document.getElementById('button vpravo').disabled = true
+	}
+
+function back() {
+	document.getElementById('button vpravo').disabled = false
+	current--
+	load_student(current)
+	console.log(current)
+	if (current == 0) {
+		document.getElementById('button vlevo').disabled = true
+	}
+}
