04b55dcb9153e37489184ca2ba2202342622259e
Konstantin <k_bobrovski03@mail.ru>
2022-03-02 11:54:32 +0700

    3

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
- let arr = []
-function generate() {
-
-let n = parseInt(document.getELementById('age').value)
-for (Let i = 0; i <n; i++) {
-
-arr[i] = Math.floor(Math.random()*10)
-}
-print()
- function zerofill() {
-
-let n = parseInt(document.getELementById('age').value)
-
-for (Let i = 0; i < n; itt) {
-arr[i] = 2
-
-}
-
-print()
- function print() {
-
-let n= parseInt (document .getELementById(‘ age) .value)|
-let div = document.getELementById(‘mes~)
-
-div.innerHTML = ''
-
-for (Let i = 0; i < n; itt) {
-div.innerHTML += arr[i] +
-
-}
+function show() {
+let div = document.getElementById('mes')
+let age = parseInt(document.getElementById('age').value)
+if (age < 10) {div.innerHTML='<h1>Вы ребенок</h1>'}
+else if (age < 17) {div.innerHTML='<h1>Вы подросток</h1>'}
+else if (age > 60) {div.innerHTML='<h1>Вы пожилой</h1>'}
+else {div.innerHTML='<h1>Вы взрослый</h1>'}
+} 
