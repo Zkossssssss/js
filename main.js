8dda7a191dde57386e2e7d1a58b93c30f9369753
Konstantin <k_bobrovski03@mail.ru>
2022-03-02 11:59:29 +0700

    2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let arr = []
-const n = 10
-for (let i=0;i<n; i++) {
-	arr[i]=Math.floor(Math.random()*10)
-}
-console.log(arr) 
+function show() {
+let age = parseInt(document.getElementById('age').value)
+if (age < 10) {console.log('Вы ребенок')}
+else if (age < 17) {console.log('Вы подросток')}
+else if (age > 60) {console.log('Вы пожилой')}
+else {console.log('Вы взрослый')}
+}  
