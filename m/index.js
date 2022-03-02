72fb61ebd995b25708808908cc2641d2a2a1ef13
Konstantin <k_bobrovski03@mail.ru>
2022-03-02 12:29:07 +0700

    table 2

--- m/index.js
+++ m/index.js
@@ -1,6 +1,5 @@
-
 let students = []
-let current = 0
+let max_id = -1
 
 function LoadFromSite() {
 	$.get('http://217.71.129.139:4035/students.php', function(data){
@@ -11,6 +10,8 @@ function LoadFromSite() {
 function show_table() {
 	let table = $('#tbl_all')
 	for (let i=0; i < students.length; i++) {
+		if (i > max_id) {
+			max_id = i
 		let tr = $('<tr></tr>')
 		
 		let td1 = $('<td>' + students[i].id + '</td>')
@@ -20,5 +21,6 @@ function show_table() {
 		tr.append(td1).append(td2).append(td3)
 		
 		table.append(tr)
+		}
 	}
 }
