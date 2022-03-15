cd8640f37e029a9d00730f10257c723cd55e2ef9
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:04:27 +0300

    }I{irnya linia

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,17 +1,12 @@
+
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
-	 ctx.beginPath();
+	ctx.beginPath();
 ctx.strokeStyle = "red";
-ctx.lineWidth = "10";
+ctx.lineWidth = "20";
 ctx.moveTo(100,150);
-ctx.lineTo(100,250);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "blue";
-ctx.lineWidth = "6";
-ctx.moveTo(200,100);
-ctx.lineTo(400,100);
-ctx.stroke(); 
+ctx.lineTo(150,75);
+ctx.lineCap = "round";
+ctx.stroke();   
 }
