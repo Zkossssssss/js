516b2f11102a62e0377817c2469b6fc2ea470cd5
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:18:09 +0300

    po chesovoi

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,3 +1,4 @@
+
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
@@ -7,6 +8,6 @@ ctx.beginPath();// начало нового пути
 ctx.lineWidth = 7; // толщина обводки
 ctx.strokeStyle = "green"; // цвет обводки
 // Координаты центра круга, радиус, начальный угол, конечный угол, направление по часовой стрелке
-ctx.arc(100, 100, 75, 0, pi/2, true );
+ctx.arc(150, 150, 75, 0, pi, true); // против часовой стрелки
 ctx.stroke(); 
 }
