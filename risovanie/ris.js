203bf6167619eefe396258c7dd986aa04547eed1
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:06:34 +0300

    }I{irnya tvo red linia

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,12 +1,12 @@
-
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
-	ctx.beginPath();
+	 ctx.beginPath();
 ctx.strokeStyle = "red";
-ctx.lineWidth = "20";
-ctx.moveTo(100,150);
-ctx.lineTo(150,75);
+ctx.lineWidth = "30";
+ctx.moveTo(100,75);
+ctx.lineTo(300,75);
+ctx.lineTo(300,200);
 ctx.lineCap = "round";
-ctx.stroke();   
+ctx.stroke(); 
 }
