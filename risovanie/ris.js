da9929447748efb2aa44e94e0cad6a6e4d4f64d4
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 19:48:48 +0300

    kvadrat s obvodkoi

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,6 +1,10 @@
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
-	ctx.fillStyle = 'pink'
-	ctx.fillRect(150, 75, 150, 150);
-} 
+	ctx.strokeStyle = "gray"; //цвет обводки
+	ctx.lineWidth = "10"; //толщина обводки
+	ctx.rect(150, 75, 225, 150);
+	ctx.stroke();
+	ctx.fillStyle = "pink"; //цвет заливки
+	ctx.fill();     
+}
