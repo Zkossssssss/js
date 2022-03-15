863bf9b5d2b8b144e95c3d57d754e7040d31ea38
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:29:43 +0300

    green line polucrug

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -2,10 +2,10 @@ function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
 
-ctx.beginPath(); // начало нового пути
+ctx.beginPath();// начало нового пути
 ctx.lineWidth = 7; // толщина обводки
 ctx.strokeStyle="green"; // цвет обводки
-ctx.moveTo(75, 150); // координаты начальной точки
-ctx.quadraticCurveTo(150, 0, 300, 150); // координаты точки искривления и конечной точки
-ctx.stroke(); // команда обводки
+ctx.moveTo(100, 150); // координаты начальной точки
+ctx.bezierCurveTo(150, 50, 340, 10, 400, 200); // координаты точки искривления и конечной точки
+ctx.stroke(); 
 }
