4b7d29c6a2327ad2bf959ad8f16ce82a0dd61b7d
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 19:53:22 +0300

    rozovii treugolnik

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,10 +1,10 @@
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
-	ctx.strokeStyle = "gray"; //цвет обводки
-	ctx.lineWidth = "10"; //толщина обводки
-	ctx.rect(150, 75, 225, 150);
-	ctx.stroke();
-	ctx.fillStyle = "pink"; //цвет заливки
-	ctx.fill();     
+	ctx.beginPath();
+	ctx.fillStyle = "pink";
+	ctx.moveTo(225,150);
+	ctx.lineTo(75,100);
+	ctx.lineTo(200,50);
+	ctx.fill();   
 }
