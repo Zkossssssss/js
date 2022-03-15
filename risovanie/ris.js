19957c2f0a3dd1d6359e71cb5c942628ac1ce014
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:19:53 +0300

    protiv no po chesovoi

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,4 +1,3 @@
-
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
@@ -8,6 +7,6 @@ ctx.beginPath();// начало нового пути
 ctx.lineWidth = 7; // толщина обводки
 ctx.strokeStyle = "green"; // цвет обводки
 // Координаты центра круга, радиус, начальный угол, конечный угол, направление по часовой стрелке
-ctx.arc(150, 150, 75, 0, pi, true); // против часовой стрелки
+ctx.arc(150, 150, 75, 0, pi, false); // по часовой стрелкe
 ctx.stroke(); 
 }
