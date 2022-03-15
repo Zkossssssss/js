e2f5c7c069eccdc48276b8cac01d52224bd4d99e
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:27:08 +0300

    green line crivy

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,22 +1,11 @@
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
- let pi = Math.PI;
 
-  ctx.beginPath();
-ctx.lineWidth = 7;
-ctx.strokeStyle = "red";
-ctx.fillStyle="red";
-ctx.arc(150, 150, 85, 0, 2*pi, false);
-ctx.stroke();
-ctx.fill();
-
-
-ctx.beginPath();
-ctx.lineWidth = 7;
-ctx.strokeStyle = "orange";
-ctx.fillStyle="orange";
-ctx.arc(250, 150, 85, 0, 2*pi, false );
-ctx.stroke();
-ctx.fill(); 
+ctx.beginPath(); // начало нового пути
+ctx.lineWidth = 7; // толщина обводки
+ctx.strokeStyle="green"; // цвет обводки
+ctx.moveTo(75, 150); // координаты начальной точки
+ctx.quadraticCurveTo(150, 0, 300, 150); // координаты точки искривления и конечной точки
+ctx.stroke(); // команда обводки
 }
