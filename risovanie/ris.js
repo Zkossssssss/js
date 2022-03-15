eb8a531019a7e089a1645d676bc49fe5e51451b7
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:35:51 +0300

    uvelichenie cvadrata

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -1,19 +1,18 @@
 
 function draw() {
-	let canva = document.getElementById('canva')
-	let ctx = canva.getContext('2d')
-
-let positionX = 75; // начальная позиция X координаты
+     let canva = document.getElementById('canva');
+let ctx = canva.getContext('2d');
+let size = 75;
 
 setInterval(function() {
     ctx.beginPath();
     ctx.clearRect(0,0,600,300); // очистка холста
     ctx.fillStyle = "pink"; // цвет заливки
-    ctx.rect(positionX, 75, 75, 75); // x, y, width, height
+    ctx.rect(75, 75, size, size); // x, y, width, height
     ctx.fill();
-    positionX++; // изменение позиции
-    if(positionX > 600) {
-        positionX = 75;
+    size++; // увеличение размера
+    if(size > 225) {
+        size = 75;
     }
-},60); // запуск каждые 60 миллисекунд
-}
+    },60); // запуск каждые 60 миллисекунд  
+} 
