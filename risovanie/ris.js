e69b17429888bc66e23623bfed7a8005db2a6566
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-15 20:24:34 +0300

    nalohenie

--- risovanie/ris.js
+++ risovanie/ris.js
@@ -3,11 +3,20 @@ function draw() {
 	let ctx = canva.getContext('2d')
  let pi = Math.PI;
 
- ctx.beginPath();
+  ctx.beginPath();
 ctx.lineWidth = 7;
-ctx.strokeStyle = "green";
-ctx.fillStyle="yellow";
+ctx.strokeStyle = "red";
+ctx.fillStyle="red";
 ctx.arc(150, 150, 85, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "orange";
+ctx.fillStyle="orange";
+ctx.arc(250, 150, 85, 0, 2*pi, false );
 ctx.stroke();
 ctx.fill(); 
 }
