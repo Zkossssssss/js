c091f7369e7d70dbf6cab4fb53898c6df4329844
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-16 12:51:34 +0300

    1 - 2

--- jq/scr.js
+++ jq/scr.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
    $("div").draggable();
 });
 $(document).ready(function() {
-   $("#drag1").draggable({axis:"x"});
-   $("#drag2").draggable({axis:"y"});
+   $("#el1").draggable({axis:"x,y"});
+});
+$(document).ready(function(){
+   $("#el1").resizable();
+}); 
+$(document).ready(function(){
+   $("#sort1").sortable();
 });
